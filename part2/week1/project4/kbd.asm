74e99463496c0d6a0ed84ceb1128448294540d58
ashishbh <ashish.a.bhat@oracle.com>
2024-10-12 12:39:16 +0530

    2.1.4: first addignment

---
+++ part2/week1/project4/kbd.asm
@@ -0,0 +1,58 @@
+(LOOP)
+    @i
+    M=0  //i=0
+
+    @SCREEN
+    D=A
+    @addr //addr=SCREEN
+    M=D
+
+    @8192
+    D=A  //8192 word = Entire display
+    @n
+    M=D
+
+    @KBD
+    D=M
+    @WHITE
+    D;JEQ
+
+
+
+    (BLACK)
+    @n
+    D=M
+    @i
+    D=D-M  //n - i
+    @LOOP
+    D;JEQ
+  
+    @addr
+    A=M
+    M=-1
+    D=A+1
+    @addr
+    M=D
+    @i
+    M=M+1
+    @BLACK
+    0;JMP
+
+    (WHITE)
+    @n
+    D=M
+    @i
+    D=D-M  //n - i
+    @LOOP
+    D;JEQ
+  
+    @addr
+    A=M
+    M=0
+    D=A+1
+    @addr
+    M=D
+    @i
+    M=M+1
+    @WHITE
+    0;JMP
